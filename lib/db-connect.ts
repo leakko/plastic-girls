5960cc8f77cfadd00a385565be76172b61d38da0
Marcos R <marcossalvo95@gmail.com>
2023-08-27 12:27:32 +0200

    Connect to mongodb

---
+++ lib/db-connect.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+export default async function() {
+	const mongoUri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.nlptsgb.mongodb.net/${process.env.MONGO_DATABASE}`;
+
+	let client: typeof mongoose;
+
+	try {
+		client = await mongoose.connect(mongoUri);
+		console.log('DB Connected');
+
+	} catch(error) {
+		console.log('Error conntecting database: ', error);
+	}
+}
