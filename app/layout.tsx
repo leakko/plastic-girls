23d1f24257bf431537cd02f767f4ec4e0c94ed5d
Marcos R <marcossalvo95@gmail.com>
2023-08-27 13:39:09 +0200

    add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import ThemeRegistry from '@/theme/ThemeRegistry';
+import NavBar from '@/components/NavBar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ThemeRegistry>
+          <NavBar />
           {children}
         </ThemeRegistry>
       </body>
